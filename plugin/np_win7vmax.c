c2023e89636736b5434d2f1ffb3638995257b5fa
Ciantic <jari.pennanen@gmail.com>
2010-06-20 22:50:02 +0300

    Initial working

---
+++ plugin/np_win7vmax.c
@@ -0,0 +1,245 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * (C)opyright 2008-2009 Aplix Corporation. anselm@aplixcorp.com
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ * ***** END LICENSE BLOCK ***** */
+
+#define WM_CHROME_WIN_CREATED WM_USER+1
+
+#include <stdio.h>
+#include <string.h>
+
+#include <windows.h>
+
+#include <npapi.h>
+#include <prtypes.h>
+#include <npfunctions.h>
+#include <npruntime.h>
+
+static NPObject        *so       = NULL;
+static NPNetscapeFuncs *npnfuncs = NULL;
+static NPP              inst     = NULL;
+static LPCWSTR			fileName = TEXT("C:\\Program Copies\\AutoHotkey\\chromeextra.hwnd");
+
+/* NPN */
+
+static void logmsg(const char *msg) {
+	FILE *out = fopen("C:\\Projects\\NPAPI\\test\\npsimple\\Release\\npsimple.log", "a");
+	if(out) {
+		fputs(msg, out);
+		fclose(out);
+	}
+}
+
+static bool
+hasMethod(NPObject* obj, NPIdentifier methodName) {
+	logmsg("npsimple: hasMethod\n");
+	return true;
+}
+
+static bool
+invokeDefault(NPObject *obj, const NPVariant *args, uint32_t argCount, NPVariant *result) {
+	logmsg("npsimple: invokeDefault\n");
+	result->type = NPVariantType_Int32;
+	result->value.intValue = 42;
+	return true;
+}
+
+static void PressKey(uint vk) {
+	INPUT* key = new INPUT();
+	key->type = INPUT_KEYBOARD;
+	key->ki.wVk = vk;
+	key->ki.dwFlags = 0;
+	key->ki.time = 0;
+	key->ki.wScan = 0;
+	key->ki.dwExtraInfo = GetMessageExtraInfo();
+
+	SendInput(1, key, sizeof(INPUT));
+}
+
+static void ReleaseKey(uint vk) {
+	INPUT* key = new INPUT();
+	key->type = INPUT_KEYBOARD;
+	key->ki.wVk = vk;
+	key->ki.dwFlags = KEYEVENTF_KEYUP;
+	key->ki.time = 0;
+	key->ki.wScan = 0;
+	key->ki.dwExtraInfo = GetMessageExtraInfo();
+
+	SendInput(1, key, sizeof(INPUT));
+}
+
+static bool
+invoke(NPObject* obj, NPIdentifier methodName, const NPVariant *args, uint32_t argCount, NPVariant *result) {
+	logmsg("npsimple: invoke\n");
+	int error = 1;
+	char *name = npnfuncs->utf8fromidentifier(methodName);
+	if(name) {
+		if(!strcmp(name, "onWindowCreated")) {
+
+			Sleep(50); // Window is not active just yet
+			HWND active = GetForegroundWindow();
+			PostMessage((HWND) active, WM_NCLBUTTONDBLCLK, HTTOP, 0);
+
+			return invokeDefault(obj, args, argCount, result);
+		} 
+	}
+	// aim exception handling
+	npnfuncs->setexception(obj, "exception during invocation");
+	return false;
+}
+
+static bool
+hasProperty(NPObject *obj, NPIdentifier propertyName) {
+	logmsg("npsimple: hasProperty\n");
+	return false;
+}
+
+static bool
+getProperty(NPObject *obj, NPIdentifier propertyName, NPVariant *result) {
+	logmsg("npsimple: getProperty\n");
+	return false;
+}
+
+static NPClass npcRefObject = {
+	NP_CLASS_STRUCT_VERSION,
+	NULL,
+	NULL,
+	NULL,
+	hasMethod,
+	invoke,
+	invokeDefault,
+	hasProperty,
+	getProperty,
+	NULL,
+	NULL,
+};
+
+/* NPP */
+
+static NPError
+nevv(NPMIMEType pluginType, NPP instance, uint16_t mode, int16_t argc, char *argn[], char *argv[], NPSavedData *saved) {
+	inst = instance;
+	logmsg("npsimple: new\n");
+	return NPERR_NO_ERROR;
+}
+
+static NPError
+destroy(NPP instance, NPSavedData **save) {
+	if(so)
+		npnfuncs->releaseobject(so);
+	so = NULL;
+	logmsg("npsimple: destroy\n");
+	return NPERR_NO_ERROR;
+}
+
+static NPError
+getValue(NPP instance, NPPVariable variable, void *value) {
+	inst = instance;
+	switch(variable) {
+	default:
+		logmsg("npsimple: getvalue - default\n");
+		return NPERR_GENERIC_ERROR;
+	case NPPVpluginNameString:
+		logmsg("npsimple: getvalue - name string\n");
+		*((char **)value) = "AplixFooPlugin";
+		break;
+	case NPPVpluginDescriptionString:
+		logmsg("npsimple: getvalue - description string\n");
+		*((char **)value) = "<a href=\"http://www.aplix.co.jp/\">AplixFooPlugin</a> plugin.";
+		break;
+	case NPPVpluginScriptableNPObject:
+		logmsg("npsimple: getvalue - scriptable object\n");
+		if(!so)
+			so = npnfuncs->createobject(instance, &npcRefObject);
+		npnfuncs->retainobject(so);
+		*(NPObject **)value = so;
+		break;
+	case NPPVpluginNeedsXEmbed:
+		logmsg("npsimple: getvalue - xembed\n");
+		*((PRBool *)value) = PR_FALSE;
+		break;
+	}
+	return NPERR_NO_ERROR;
+}
+
+static NPError /* expected by Safari on Darwin */
+handleEvent(NPP instance, void *ev) {
+	inst = instance;
+	logmsg("npsimple: handleEvent\n");
+	return NPERR_NO_ERROR;
+}
+
+static NPError /* expected by Opera */
+setWindow(NPP instance, NPWindow* pNPWindow) {
+	inst = instance;
+	logmsg("npsimple: setWindow\n");
+	return NPERR_NO_ERROR;
+}
+
+/* EXPORT */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+NPError OSCALL
+NP_GetEntryPoints(NPPluginFuncs *nppfuncs) {
+	logmsg("npsimple: NP_GetEntryPoints\n");
+	nppfuncs->version       = (NP_VERSION_MAJOR << 8) | NP_VERSION_MINOR;
+	nppfuncs->newp          = nevv;
+	nppfuncs->destroy       = destroy;
+	nppfuncs->getvalue      = getValue;
+	nppfuncs->event         = handleEvent;
+	nppfuncs->setwindow     = setWindow;
+
+	return NPERR_NO_ERROR;
+}
+
+#ifndef HIBYTE
+#define HIBYTE(x) ((((uint32)(x)) & 0xff00) >> 8)
+#endif
+
+NPError OSCALL
+NP_Initialize(NPNetscapeFuncs *npnf)
+{
+	logmsg("npsimple: NP_Initialize\n");
+	if(npnf == NULL)
+		return NPERR_INVALID_FUNCTABLE_ERROR;
+
+	if(HIBYTE(npnf->version) > NP_VERSION_MAJOR)
+		return NPERR_INCOMPATIBLE_VERSION_ERROR;
+
+	npnfuncs = npnf;
+	return NPERR_NO_ERROR;
+}
+
+NPError
+OSCALL NP_Shutdown() {
+	logmsg("npsimple: NP_Shutdown\n");
+	return NPERR_NO_ERROR;
+}
+
+char *
+NP_GetMIMEDescription(void) {
+	logmsg("npsimple: NP_GetMIMEDescription\n");
+	return "application/x-vertical-maximize:.foo:jari.pennanen@gmail.com";
+}
+
+NPError OSCALL /* needs to be present for WebKit based browsers */
+NP_GetValue(void *npp, NPPVariable variable, void *value) {
+	inst = (NPP)npp;
+	return getValue((NPP)npp, variable, value);
+}
+
+#ifdef __cplusplus
+}
+#endif
